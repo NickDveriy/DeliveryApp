f07268a1dd22b5463fcc6650acba65381a974f66
NickDveriy <nick.dveriy95@gmail.com>
2021-06-14 22:44:32 +0300

    Fix app.get.

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,7 @@ const app: Express = express();
 
 
 app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
-});
+
 
 const PORT: string | number = process.env.PORT || 4000;
 
@@ -22,6 +20,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(driverRoutes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+});
 
 const mongoUri: string = `mongodb+srv://nickDV:PmM4eYzMCIakZuww@jarviscluster.vjbi4.mongodb.net/deliveryDb?retryWrites=true&w=majority`;
 const mongoOptions = { useNewUrlParser: true, useUnifiedTopology: true };
